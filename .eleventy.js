965018239d19fc0fd95403c13e2cd97b16d71d69
sylbru <sylvain.brunerie@gmail.com>
2024-09-14 00:24:09 +0200

    live-reload when changing CSS file

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
 }
