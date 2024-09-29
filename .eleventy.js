d044d9a67c35b1180dacab0a4d9d0bd0f73d6486
sylbru <sylvain.brunerie@gmail.com>
2024-09-29 03:17:47 +0200

    Add warning about the state of CSS watching

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ const CleanCss = require("clean-css");
 module.exports = function (eleventyConfig) {
   eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
   eleventyConfig.addPassthroughCopy("img");
+
+  // This makes the dev server rebuild when anything
+  // changes in a CSS file, but does not reload the
+  // page in the browser.
   eleventyConfig.addWatchTarget("css/");
 
   eleventyConfig.addFilter("cssmin", function (code) {
